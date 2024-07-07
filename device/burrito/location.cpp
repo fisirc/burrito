6705c0d9160af17c2da63ebd76d373f2fbfe2a2f
Paolo Flores <pflores.fisi22@gmail.com>
2024-07-07 18:33:22 -0500

    break on timeout (gps)

--- device/burrito/location.cpp
+++ device/burrito/location.cpp
@@ -5,7 +5,7 @@
 bool gps_delay(TinyGPSPlus* gps, unsigned long ms) {
   unsigned long start = millis();
   do {
-    while (GPS_SERIAL.available() > 0) {
+    while (GPS_SERIAL.available() > 0 && millis() - start < ms) {
       byte read = GPS_SERIAL.read();
       // This debugging branch prints the NMEA in the programming serial
       if (DEBUG_GPS_NMEA) {
@@ -16,5 +16,6 @@ bool gps_delay(TinyGPSPlus* gps, unsigned long ms) {
       }
     }
   } while (millis() - start < ms);
+  Serial.print('\n');
   return false;
 }
